70ecb4fb79527059e015af99b10860dc18ad6e90
Žan Grajfoner <zan.grajfoner@student.um.si>
2023-12-16 17:56:02 +0100

    modify next.js workflow

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src= {`${prefix}vercel.svg`}
+              src= {`kmetija-jelincic/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
